4a371f0161273f8b9133c47bf34ff877488fcee2
BlueManiac <bluemaniac@users.noreply.github.com>
2019-01-09 21:06:21 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,63 @@
+var router = new VueRouter({
+    mode: 'history',
+    routes: []
+});
+
+new Vue({
+    router,
+    data() {
+        return {
+            genders: [{
+                type: "Men",
+                count: 0
+            }, {
+                type: "Women",
+                count: 0
+            }],
+            history: []
+        }
+    },
+    created() {
+        for (let gender of this.genders) {
+            gender.count = parseInt(localStorage.getItem(gender.type) || 0);
+        }
+
+        this.$mount('#app');
+    },
+    mounted() {
+    },
+    methods: {
+        increase(gender, count) {
+            let change = Math.max(-gender.count, count);
+
+            if (!change)
+                return;
+
+            gender.count += change;
+
+            this.save(gender, change);
+        },
+        reset() {
+            for (let gender of this.genders) {
+                gender.count = 0;
+
+                this.save(gender, "Reset");
+            }
+        },
+        save(gender, change) {
+            this.history.unshift({
+                time: new Date(),
+                gender: gender.type,
+                change: change
+            });
+            localStorage.setItem(gender.type, gender.count);
+        }
+    },
+    computed: {
+        total() {
+            return this.genders.reduce((sum, y) => sum + y.count, 0);
+        }
+    },
+    watch: {
+    }
+})
